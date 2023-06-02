3baa0d65468f22a9a678ac6f4aef1bf85a13d13e
ohurjon <ohurjon@gmail.com>
2023-06-02 09:24:43 +0900

    Update File System

--- main.py
+++ main.py
@@ -30,34 +30,43 @@ if __name__ == "__main__":
 
     code = 200
 
-    link = input("유튜브 링크나 영상 id를 입력해주세요. : ")
+    f = open("data/data.txt", 'w')
+    f.close()
 
-    if link.startswith("http"):
-        id = link.split("=")[1]
-    elif len(link) == 11:
-        id = link
-    else:
-        code = 400
-        raise ValueError
+    # path = input("유튜브 링크나 영상 id가 적힌 TXT 파일의 경로를 입력해주세요. : ")
 
-    if code == 200:
-        print("입력된 영상 id : " + id)
-        nouns = {}
+    with open("data/path.txt", "r") as f:
+        links = [N.strip() for N in f.readlines()]
+    f.close()
 
-        for i in getData(videoID=id)["items"]:
-            comment = i["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
-            # print(comment)
+    for link in links:
+        if link.startswith("http"):
+            id = link.split("=")[1]
+        elif len(link) == 11:
+            id = link
+        else:
+            code = 400
+            raise ValueError
 
-            for noun in kkma.nouns(comment):
-                # print(noun,end="_")
-                if len(noun) >= 2:
-                    if not nouns.keys().__contains__(noun):
-                        nouns[noun] = 1
-                    else:
-                        nouns[noun] += 1
+        if code == 200:
+            print("입력된 영상 id : " + id)
+            nouns = {}
 
-        for i in list(nouns.keys()):
-            if i in data.abuse:
-                del nouns[i]
+            for i in getData(videoID=id)["items"]:
+                comment = i["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
 
-        print(sorted(nouns.items(), key=lambda item: item[1], reverse=True))
+                for noun in kkma.nouns(comment):
+
+                    if len(noun) >= 2:
+                        if not nouns.keys().__contains__(noun):
+                            nouns[noun] = 1
+                        else:
+                            nouns[noun] += 1
+            for i in list(nouns.keys()):
+                if i in data.abuse:
+                    del nouns[i]
+            sorted_data = sorted(nouns.items(), key=lambda item: item[1], reverse=True)
+            print(sorted_data)
+            f = open("data/data.txt", 'a')
+            f.write(f"{id} \n {sorted_data} \n")
+            f.close()
