56bd564b4a9640dc513df6d885f85bf42051b68e
Lee <yesmars337@gmail.com>
2023-06-07 23:30:07 +0900

    자바 메모리 문제로 인하여 선 저장 후 정렬 방식으로 수정 댓글 + 좋아요 수, 댓글 + 빈도수, 댓글 + 빈도수*좋아요 수 리스트 기능 추가

--- main.py
+++ main.py
@@ -1,72 +1,100 @@
 import googleapiclient.discovery
 from konlpy.tag import Kkma
-
-import data
 import key
+import re
 
 kkma = Kkma()
+max_comments = 500
 
-
-def getData(videoID="", pageToken=""):
+def getData(max_comments, videoID="", pageToken=""):
     api_service_name = "youtube"
     api_version = "v3"
     DEVELOPER_KEY = key.YOUTUBE_API_KEY
+    comments = []
 
     youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+    while len(comments) < max_comments:
+        request = youtube.commentThreads().list(
+            pageToken=pageToken,
+            part="snippet,replies",
+            maxResults=min(100, max_comments - len(comments)),
+            textFormat="plainText",
+            videoId=videoID
+        )
+        response = request.execute()
 
-    request = youtube.commentThreads().list(
-        pageToken=pageToken,
-        part="snippet",
-        maxResults=100,
-        textFormat="plainText",
-        videoId=videoID
-    )
-    response = request.execute()
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textOriginal']
+            like_count = item['snippet']['topLevelComment']['snippet']['likeCount']
 
-    return response
+            comment = ' '.join(re.findall(r'[0-9\u3131-\u3163\uac00-\ud7a3]+', comment))
+            comments.append((comment, like_count))
+
+        if 'nextPageToken' in response:
+            pageToken = response['nextPageToken']
+        else:
+            break
+
+    return comments[:max_comments]
+
+def scoring_comments(data):
+    kkma = Kkma()
+    count = {}
+    count_x_likes = {}
+
+    for comment, likes in data:
+        nouns = kkma.nouns(comment)
+        for noun in nouns:
+            if not count.keys().__contains__(noun):
+                count[noun] = 1
+            else:
+                count[noun] += 1
+            if not count_x_likes.keys().__contains__(noun):
+                count_x_likes[noun] = likes
+            else:
+                count_x_likes[noun] += likes
+
+    return count, count_x_likes
 
 
 if __name__ == "__main__":
 
     code = 200
 
-    f = open("data/data.txt", 'w')
+    f = open("data.txt", 'w')
     f.close()
 
-    # path = input("유튜브 링크나 영상 id가 적힌 TXT 파일의 경로를 입력해주세요. : ")
-
-    with open("data/path.txt", "r") as f:
+    with open("path.txt", "r") as f:
         links = [N.strip() for N in f.readlines()]
     f.close()
 
     for link in links:
         if link.startswith("http"):
-            id = link.split("=")[1]
+            id = link.split("=")[1].split("&")[0]
         elif len(link) == 11:
             id = link
         else:
+            print("에러")
             code = 400
-            raise ValueError
+
+        print(id)
 
         if code == 200:
-            print("입력된 영상 id : " + id)
-            nouns = {}
 
-            for i in getData(videoID=id)["items"]:
-                comment = i["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
+            # max_comments = int(input("어느만큼 댓글을 수집하시겠습니까? : "))
+            max_comments = 500
+            data = getData(max_comments, videoID=id)
 
-                for noun in kkma.nouns(comment):
+            sort_data = sorted(data, key=lambda x: x[1], reverse=True)
+            print(sort_data,"\n\n")
 
-                    if len(noun) >= 2:
-                        if not nouns.keys().__contains__(noun):
-                            nouns[noun] = 1
-                        else:
-                            nouns[noun] += 1
-            for i in list(nouns.keys()):
-                if i in data.abuse:
-                    del nouns[i]
-            sorted_data = sorted(nouns.items(), key=lambda item: item[1], reverse=True)
-            print(sorted_data)
-            f = open("data/data.txt", 'a')
-            f.write(f"{id} \n {sorted_data} \n")
+            count, count_x_likes = analyze_comments(data)
+
+            sort_count = sorted(count.items(), key=lambda item: item[1], reverse=True)
+            sort_count_x_likes = sorted(count_x_likes.items(), key=lambda item: item[1], reverse=True)
+
+            print(sort_count, "\n\n", sort_count_x_likes, "\n\n\n")
+
+            f = open("data.txt",'a')
+            f.write(f"{id} \n\n {sort_data} \n\n {sort_count} \n\n {sort_count_x_likes} \n\n\n")
             f.close()
